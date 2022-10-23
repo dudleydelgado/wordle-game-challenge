6a96d39cd518dcd33fd8b53b65841dd3ec841380
dudley delgado <arturobonilla0412@gmail.com>
2022-10-24 01:28:25 +0200

    Create new Rows,fixed errors about deleteContentBackward, create array with the new lastFiveSquareFilled and add message Ganaste o perdiste

--- main.js
+++ main.js
@@ -22,46 +22,61 @@ function listenInput(actualRow){
 //Por cada elemento squares vamos a escuchar cuando alguien ingrese algo en su input
 squares.forEach(element => {
     element.addEventListener('input', event=>{
-        //Recoger el ingreso del usuario, este .target.value es una propiedad que pertenece a los input y es como se puede recoger lo que el usuario escribe en el input
-        userInput.push(event.target.value.toUpperCase())
-        console.log(userInput)
-        if(event.target.nextElementSibling){
-            event.target.nextElementSibling.focus();
-        } else{
+        //Si no se ha borrado
+        if(event.inputType !== 'deleteContentBackward'){
+            //Recoger el ingreso del usuario, este .target.value es una propiedad que pertenece a los input y es como se puede recoger lo que el usuario escribe en el input
+            userInput.push(event.target.value.toUpperCase())
+            if(event.target.nextElementSibling){
+                event.target.nextElementSibling.focus();
+            } else{
+
+            //Crear arreglo con las letras llenas
+
+            //Buscar el contenido de la fila anterior
+            //Armar un arreglo con el resultado antes de comparar
+            let squaresFilled = document.querySelectorAll('.square')
+            squaresFilled = [...squaresFilled]
+            let lastFiveSquaresFilled = squaresFilled.slice(-5);
+            let finalUserInput = [];
+            lastFiveSquaresFilled.forEach(element => {
+               finalUserInput.push(element.value.toUpperCase())
+            });
+
 
             // Cambiar estilos si existe la letra pero no esta en la poscion correcta
-           let existIndexArray = existLetter(wordArray, userInput)
-           existIndexArray.forEach(element => {
-            squares[element].classList.add('gold');
-           });
-
-            //Comparar arreglos para cambiar estilos
-           let rightIndex = compareArrays(wordArray, userInput)
-           //Todos los elementos que tengan el indice correcto le vamos a agregar la clase green
-           rightIndex.forEach(element => {
-            squares[element].classList.add('green');
-           })
-           //Si los arreglos son iguales
-
-           if(rightIndex.length == wordArray.length){
-            resultElement.innerHTML = `
-            <p>Ganaste</p>
-            <button class="button">Reiniciar</button>`
-
-            let resetBtn = document.querySelector('.button')
-
-            resetBtn.addEventListener('click', () => {
-                location.reload();
+            let existIndexArray = existLetter(wordArray, finalUserInput)
+            existIndexArray.forEach(element => {
+                squares[element].classList.add('gold');
             });
-            return;
-           }
 
-           //Crear una nueva fila
-           let actualRow =  createRow()
-           drawSquares(actualRow)
-           listenInput(actualRow)
-           addFocus(actualRow)
+                //Comparar arreglos para cambiar estilos
+            let rightIndex = compareArrays(wordArray, finalUserInput)
+            //Todos los elementos que tengan el indice correcto le vamos a agregar la clase green
+            rightIndex.forEach(element => {
+                squares[element].classList.add('green');
+            })
+            //Si los arreglos son iguales
 
+            if(rightIndex.length == wordArray.length){
+                showResult('Ganaste!')
+
+                return;
+            }
+
+            //Crear una nueva fila
+            let actualRow =  createRow()
+
+                if(!actualRow){
+                    return
+                }
+
+            drawSquares(actualRow)
+            listenInput(actualRow)
+            addFocus(actualRow)
+
+            }
+        }else{
+            userInput.pop();
         }
     })
 })
@@ -76,10 +91,9 @@ let iqualsIndex = []
 
     array1.forEach((element, index) => {
         if(element == array2[index]){
-            console.log(`En la posición ${index} si son iguales`);
             iqualsIndex.push(index);
         } else{
-            console.log(`En la posición ${index} NO son iguales`);
+            
         }
     });
     return iqualsIndex;
@@ -97,11 +111,15 @@ function existLetter(array1, array2){
 
 function createRow(){
     rowId++
-    let newRow = document.createElement('div');
-    newRow.classList.add('row');
-    newRow.setAttribute('id', rowId)
-    mainContainer.appendChild(newRow)
-    return newRow;
+    if(rowId <= 5){
+        let newRow = document.createElement('div');
+        newRow.classList.add('row');
+        newRow.setAttribute('id', rowId)
+        mainContainer.appendChild(newRow)
+        return newRow;
+    }else{
+        showResult(`Intentalo de nuevo la respuesta correcta era "${word.toUpperCase()}"`)
+    }
 }
 
 function drawSquares(actualRow){
@@ -117,4 +135,15 @@ function drawSquares(actualRow){
 function addFocus(actualRow){
     let focusElement = actualRow.querySelector('.focus')
     focusElement.focus();
+}
+
+function showResult(textMsg){
+    resultElement.innerHTML = `
+    <p>${textMsg}</p>
+    <button class="button">Reiniciar</button>`
+
+    let resetBtn = document.querySelector('.button')
+    resetBtn.addEventListener('click', () => {
+        location.reload();
+    });
 }
